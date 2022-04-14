3ad4f662e05b5145250bc583d40a6ea2771415b0
TheLucius <kirito.lc@gmail.com>
2022-04-14 19:04:29 +0200

    gcd added and first gcd tests

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,38 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
+int gcd(int a, int b){
+    int sol;
+    int temp;
+    if(a<=b){
+        temp = a;
+    } else{
+        temp = b;
+    }
+
+    for (int x=1; x<=temp; x++){
+        if(a % x ==0){
+            if (b % x == 0){
+                sol = x;
+            }
+        }
+    }
+    return sol;
+
+}
+
+TEST_CASE("calc_gcd", "[gcd]")
+{
+    REQUIRE(gcd(2,4)== 2);
+    REQUIRE(gcd(6,9)== 3);
+    REQUIRE(gcd(3,7)== 1);
+
+}
+
+
+
 int main(int argc, char* argv[])
 {
   return Catch::Session().run(argc, argv);
 }
+
