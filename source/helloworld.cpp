88b51cc6a3213a63da90528dd66660e49d1727d0
TheLucius <kirito.lc@gmail.com>
2022-04-14 19:02:52 +0200

    Gruß an Anton und 1 to 20

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+int one_to_twenty(){
+    for (int i = 21; i>20; i++){
+        for (int x = 20; x < 21; x--) {
+            if (i % x != 0){
+                break;
+            } else
+            {
+                if (x == 2){
+                    return i;
+                }
+            }
+        }
+    }
+}
+
+
 int main()
 {
-  std::cout << "Hello, World!\n";
+  std::cout << "Hello, Anton!\n";
+  std::cout << one_to_twenty();
   return 0;
 }
