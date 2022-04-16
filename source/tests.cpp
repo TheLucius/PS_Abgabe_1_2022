526738c00205eacd69ed8fad9d2d0cfce082e098
TheLucius <kirito.lc@gmail.com>
2022-04-16 16:12:38 +0200

    added sum_mult and first tests

--- source/tests.cpp
+++ source/tests.cpp
@@ -21,12 +21,49 @@ int gcd(int a, int b){
 
 }
 
+unsigned int checksum (int a){                  //checksum function
+    unsigned int sum;
+    std::string splitted = std::to_string(a);       //split input into string
+    for (unsigned int x = 0; x<splitted.length();x++){  //iterate over string
+        sum += splitted[x] - '0';                             //added all single charachters
+    }
+    return sum;
+}
+
+unsigned int sum_multiplies(unsigned int limit){    //sum mult function
+    unsigned int sum=0;
+    for(int x=0; x<=limit; x++){                    //count up to limit
+        if (x % 3 == 0) {                           //check if divideable by 3
+            sum = x + sum;
+        }
+        else if(x % 5 == 0){                        //check if divedable by 5
+            sum = x + sum;
+        }
+    }
+    return sum;
+}
+
 TEST_CASE("calc_gcd", "[gcd]")
 {
     REQUIRE(gcd(2,4)== 2);
     REQUIRE(gcd(6,9)== 3);
     REQUIRE(gcd(3,7)== 1);
 
+}
+
+TEST_CASE("calc_checksum", "[cs]")
+{
+    REQUIRE(checksum(123) == 6);
+    REQUIRE(checksum(121808) == 20);
+    //REQUIRE(checksum(-120) == 1);
+}
+
+TEST_CASE("calc_sum_of_multiplies", "[summult]")
+{
+    REQUIRE(sum_multiplies(10)==33);
+    REQUIRE(sum_multiplies(20)==98);
+    REQUIRE(sum_multiplies(1000)==234168);
+    //REQUIRE(sum_multiplies(-1)==1);
 }
 
 
